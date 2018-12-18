4e244b3179ba368ace9409acfd6cf677074cdaf5
tkubik <tkubik@192.168.52.107>
2018-12-18 10:35:23 +0100

    Image class created. Model constructor added.

---
+++ Image.m
@@ -0,0 +1,40 @@
+classdef Image
+    %IMAGE Representation of the image:
+    %   voxel matrices
+    %   image dimensions
+    %   voxel size
+    %   time vector
+    %   image origin (point 0,0,0)
+    
+    properties
+        time; % 1-D matrix containing time values in seconds
+        dim; % 1-D 4 element matrix containing dimensions: [nx ny nz nt]
+        voxelSize; % 1-D 3 element matrix containg voxel sizes in direction: [dx dy dz]
+        imageOrigin; %  1-D 3 element matrix containg lication of the image origin: [ox oy oz]
+        voxels; % 4-D matrix containg all image voxels: [time z x y];
+    end
+    
+    methods
+        function obj = Image(nx,ny,nz,nt)
+            %IMAGE Constructs an empty image of the given dimensions.
+            obj.voxels = zeros(nt,nz,nx,ny);
+            obj.dim = [nx ny nz nt];
+            obj.voxelSize = [1 1 1];
+            obj.imageOrigin = [0 0 0];
+            obj.time = 1:nt;
+        end
+        
+        function obj = readImage(imageFile)
+            % TODO for Kasia: readImage from file
+        end
+        
+        function [ox oy oz] = getRealFromMatrix(ix, iy, iz)
+            % TODO for Kasia: convert matrix coordinates into milimeters
+        end;
+        
+        function [ox oy oz] = getMatrixFromReal(ix, iy, iz)
+            % TODO for Kasia: convert matrix coordinates into milimeters
+        end;
+    end
+end
+
--- Model.m
+++ Model.m
@@ -3,16 +3,23 @@ classdef Model
     %   Class represents the segmentation model streched over radially
     %   equal nodes and approximated by Bsplines.
     
-    properties (GetAccess = private)
+    properties
         middle; % Middle point - should be 1D array.
-        r;
+        % Model coordinates:
+        thetas;
+        phis;
+        rs;
     end
     
     methods
-        function obj = Model(x,y,z)
+        function obj = Model(x,y,z,ntheta,nphi)
             %MODEL Construct an instance of this class
-            %   Model is initialized with its middle coordinates.
+            %   Model is initialized with its middle coordinates and with the specific radial
+            %   spacing (ntheta = 0 means model 2D).
             obj.middle = [x y z];
+            obj.thetas = linspace(0,180,ntheta+1);
+            obj.phis = linspace(0,360,nphi+1);
+            obj.rs = ones(ntheta+1,nphi+1);
         end
         
         function [mid] = getMiddle(obj)
@@ -20,6 +27,14 @@ classdef Model
             mid(1) = obj.middle(1);
             mid(2) = obj.middle(2);
             mid(3) = obj.middle(3);
+        end
+        
+        function obj = setR(obj,nth,nph,r)
+            rs(nth,nph) = r;
+        end
+        
+        function r = getR(obj,nth,nph)
+            rs(nth,nph);
         end
     end
 end
