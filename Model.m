f96951872bff888021d5a6e7f7786269acae1af3
tkubik <tkubik@mbp-tomasz>
2019-07-31 11:33:28 +0200

    Wyrównanie z kodem Kasi

--- Model.m
+++ Model.m
@@ -9,8 +9,10 @@ classdef Model
         thetas;
         phis;
         rs;
+        bspline;
         image Image
         app app1
+        neighbourhood
     end
     
     methods (Access = public)
@@ -22,9 +24,10 @@ classdef Model
             obj.thetas = linspace(0,180,ntheta+1);
             obj.phis = linspace(0,360,nphi+1);
             obj.rs = ones(ntheta+1,nphi+1);
+            obj.neighbourhood = [5,5];
         end
         
-        function [xunit, yunit, obj] = create2DModelBasedOnElipse(obj,image,x,y,z,ra,rb,ntheta,nphi)
+        function [xunit, yunit, obj] = create2DModelBasedOnElipse(obj,image,x,y,z,ra,rb,ntheta,nphi,valueInMM)
             obj = Model(x,y,z,ntheta,nphi);
             obj.image = image;
       %      obj = Model (1,1,1,1,1);   
@@ -48,24 +51,104 @@ classdef Model
             
             xunit = (ra * cos(obj.phis) / image.voxelSize(2)) + ix;
             yunit = (rb * sin(obj.phis) / image.voxelSize(1)) + iy;
-             % TODO: Geometrycne s³upki
             
-            
+            knots = [obj.phis];
+            knots = [-knots(2) knots knots(length(knots))+knots(2)];
+            weights = obj.rs;
+            obj.bspline = fastBSpline(knots,weights);
+            obj.neighbourhood = createNeighbourhood(obj, valueInMM);
+            test = 1;
+        end
+       
+        function [Xm, Ym] = calculateCoordinatesOfTheNode2D(obj,n)
+                r = evalAt(obj.bspline,obj.phis(n));
+                Xm = r*cos(obj.phis(n));
+                Ym = r*sin(obj.phis(n));
         end
         
-        function [Xm, Ym] = calculateCoordinatesOfTheNode2D(obj, nphi, phis, rs)
-            obj = Model(x,y,z,ntheta,nphi);
-            phis = obj.phis;
-            rs = obj.rs;
-            for i = 1:nphi+1
-                Xm = obj.r(i)*cos(obj.phis(i));
-                Ym = obj.r(i)*sin(obj.phis(i));
+        function [R, Phi] = calculateCartesianToPolar(obj, Xm,Ym)
+            R = sqrt((Xm-obj.middle(1)).^2 + (Ym-obj.middle(2)).^2);
+            Phi = atan((Ym-obj.middle(2))/(Xm-obj.middle(1))); 
+        end
+             
+        function location = isPixelInOrOut(obj, x, y)
+            epsi = 10^(-5);
+            [R, Phi] = calculateCartesianToPolar(obj, x,y);
+            if R == 0
+                Phi = 0;   
             end
-               
+            modelR = evalAt(obj.bspline,Phi);
+            location = 1/2*(1+2/pi*atan((R-modelR)/epsi));          
         end
         
-        function isPixelInOrOut(obj)
-          
+        function [u,v] = calculateAvrVOxelsIntensiti(obj, n, neighbourhood) %s¹siedztwo + pêtla po ca³oœci 
+            [xn, yn] = calculateCoordinatesOfTheNode2D(obj,n);
+            sumInside = 0;
+            sumOutside = 0;
+            weightsInside = 0;
+            weightsOutside = 0;
+            xn=round(xn);
+            yn=round(yn);
+            for x=xn-neighbourhood(2):1:xn+neighbourhood(2)
+                for y=yn-neighbourhood(1):1:yn+neighbourhood(1)
+                    if x<1 || y<1 || x>obj.image.dim(1) || y>obj.image.dim(2)
+                        continue
+                    end
+                    hv = isPixelInOrOut(obj,x,y);
+                    sumInside = sumInside + obj.image.voxels(1,1,y,x)*hv;
+                    sumOutside = sumOutside + obj.image.voxels(1,1,y,x)*(1-hv);
+                    weightsInside = weightsInside + hv;
+                    weightsOutside = weightsOutside + (1-hv);
+                end
+            end
+            u = sumInside/weightsInside; 
+            v = sumOutside/weightsOutside;
+        end
+         
+        function fInside = fIn(obj,n,u, neighbourhood)
+            [xn, yn] = calculateCoordinatesOfTheNode2D(obj,n);
+            fInside = 0;
+            xn = round(xn);
+            yn = round(yn);
+            for x=xn-neighbourhood(2):1:xn+neighbourhood(2)
+                for y=yn-neighbourhood(1):1:yn+neighbourhood(1)
+                     if x<1 || y<1 || x>obj.image.dim(1) || y>obj.image.dim(2)
+                        continue
+                    end
+                    fInside = fInside + (obj.image.voxels(1,1,y,x) - u)^2;
+                end
+            end
+        end
+            
+           
+        function fOutside = fOut(obj,n, v, neighbourhood)
+            [xn, yn] = calculateCoordinatesOfTheNode2D(obj,n);
+            xn = round(xn);
+            yn = round(yn);
+            fOutside = 0;
+            for x=xn-neighbourhood(2):1:xn+neighbourhood(2)
+                for y=yn-neighbourhood(1):1:yn+neighbourhood(1)
+                     if x<1 || y<1 || x>obj.image.dim(1) || y>obj.image.dim(2)
+                        continue
+                    end
+                    fOutside = fOutside + (obj.image.voxels(1,1,y,x) - v)^2;
+                end
+            end
+        end
+        
+        function energy = energyOfModel(obj)   %w pêtli po wêz³ach
+            energy = 0;
+            for n = 1:length(obj.phis)
+                [u, v] = calculateAvrVOxelsIntensiti(obj, n, obj.neighbourhood);
+                fInside = fIn(obj,n,u, obj.neighbourhood);
+                fOutside = fIn(obj,n,v, obj.neighbourhood);
+                energy = energy + fInside + fOutside;
+            end
+            disp(['Energy of the model is: ', num2str(energy)])
+        end
+        
+        function neighbourhood = createNeighbourhood(obj, valueInMM)
+            neighbourhood = [round(valueInMM/obj.image.voxelSize(2)), round(valueInMM/obj.image.voxelSize(1))];  
             
         end
         
@@ -74,7 +157,6 @@ classdef Model
             mid(1) = obj.middle(1);
             mid(2) = obj.middle(2);
             mid(3) = obj.middle(3);
-       
         end
         
         function obj = setR(obj,nth,nph,r)
